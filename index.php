0a77c99df04b0c9d51938e10529abbc0690ade50
Luis Valencia <lvalencia@csumb.edu>
2018-02-11 21:04:13 +0000

    Slot Machine

---
+++ index.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<?php
+        include 'inc/functions.php';
+        ?>
+<html>
+    <head>
+        <title> 777 Slot Machine </title>
+        <style>
+            @import url("css/styles.css");
+        </style>
+    </head>
+    <body>
+        <main>
+            
+        <?php
+        play();
+        ?>
+        
+        <form>
+            <input type="submit" value="Spin!"/>
+        </form>
+        
+        </main>
+        
+    </body>
+</html>
